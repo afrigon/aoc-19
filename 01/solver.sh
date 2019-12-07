11ac69892e3e8efee35abb83da6f9fb5901fe590
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-12-07 18:40:47 -0500

    day 1 part 2 using awk

--- 01/solver.sh
+++ 01/solver.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env sh
-awk '{s+=int($1/3)-2} END {print s}' input.txt
+awk '{ s += int($1 / 3) - 2 } END {print "part 1: "s}' input.txt
+
+awk '{
+    x = $1;
+    do { 
+        x = int(x / 3) - 2;
+        s += x > 0 ? x : 0
+    } while (x > 0)
+} END { print "part 2: "s }' input.txt
+
